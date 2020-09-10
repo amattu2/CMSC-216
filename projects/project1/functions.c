3d823a1371b78ec35f1f78dc9a1f399fbc7ac7f0
= <alecmattu1234@gmail.com>
2020-09-10 12:27:43 -0400

    Add project 1

---
+++ projects/project1/functions.c
@@ -0,0 +1,34 @@
+/*
+ * CMSC 216 (Fall 2020)
+ * Section: 0303 (M/W 8am)
+ * University ID: 117568813
+ * Directory ID: amattu
+ *
+ * Author: Alec M.
+ * Date:
+ *
+ * I pledge on my honor that I have not given or received
+ * any unauthorized assistance on this assignment.
+*/
+
+/* Files */
+#include <stdio.h>
+
+/*
+ * Determine if given year has 365/366 days
+ *
+ * @param int year
+ * @return int -1/1
+*/
+int has366(int year) {
+	return 0;
+}
+
+/*
+ * Calculate the sum of positive divisors
+ *
+ * @param int number
+ */
+long sopd(int num, int n) {
+	return 0;
+}
