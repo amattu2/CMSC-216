454ec8cac138e8dd85051893f6ff2c37b45708f5
= <alecmattu1234@gmail.com>
2020-09-10 19:51:46 -0400

    Added helper functions and base code

--- projects/project1/functions.c
+++ projects/project1/functions.c
@@ -18,17 +18,53 @@
  * Determine if given year has 365/366 days
  *
  * @param int year
- * @return int -1/1
+ * @return int -1 (invalid) / 0 (365d) / 1 (366d)
 */
 int has366(int year) {
-	return 0;
+	/* Checks */
+	if (year <= 1752) { return -1; }
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 /*
  * Calculate the sum of positive divisors
  *
- * @param int number
- */
+ * @param int -1 (invalid) / sum
+*/
 long sopd(int num, int n) {
-	return 0;
+	/* Checks */
+	if (num < 1 || n < 0) { return -1; }
+
+	/* Variables */
+	long sum = 0;
+
+	/* Return */
+	return sum;
+}
+
+/*
+ * Calculate the power of a variable
+ *
+ * @param long base
+ * @param int exponent
+ *
+ * printf("%zu", power(2, 2));
+*/
+long power(long base, int exponent) {
+	/* Variables */
+	long sum = base;
+
+	/* Loops */
+	for (int i = 0; i < exponent; i++) {
+		printf("%1d\n", sum);
+		sum *= base;
+	}
+
+	/* Return */
+	printf("Result %1d\n", sum);
+	return sum;
 }
