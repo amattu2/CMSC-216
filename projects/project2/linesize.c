1c7336aacce943bc7130fa50a0a943a3bbe039ae
= <alecmattu1234@gmail.com>
2020-09-20 10:25:44 -0400

    Attempt to implement parseWhitespace, finish other project requirements

--- projects/project2/linesize.c
+++ projects/project2/linesize.c
@@ -5,17 +5,27 @@
  * Directory ID: amattu
  *
  * Author: Alec M.
- * Date: Sept 17th, 2020
+ * Date: Sept 20th, 2020
  *
  * I pledge on my honor that I have not given or received
  * any unauthorized assistance on this assignment.
 */
 
+/**
+	Current issues:
+	- According to the public tests, \n shouldn't be counted... And when it's on it's own line
+	it is being counted
+	- With the exception of the first line, most of them have differ in length by 1
+	- Tabs are miscounted horribly
+ */
+
+
+
 /* Files */
 #include <stdio.h>
 
 /* Prototypes */
-void parseLine(char line[], int size);
+int parseLine(char line[], int size);
 int parseWhitespace(char line[], int size);
 void reset(char *array, int size);
 
@@ -25,34 +35,43 @@ void reset(char *array, int size);
  * @param char argument count
  * @param char argument vector
  * @return int status code
- * @author Alec M. <https://amattu.com>
+ * @author Alec M.
  * @date 2020-09-17T12:54:12-040
  */
 int main(int argc, char *argv[])  {
 	/* Variables */
 	char line[999];
-	int i = 0;
+	int array_index = 0;
+	int lines_exceeding = 0;
+	int line_count = 0;
 
 	/* Iterate Through Input */
 	while (feof(stdin) != 1) {
 		/* Read Character */
-		scanf("%c", &line[i]);
+		scanf("%c", &line[array_index]);
 
 		/* Checks for EOL */
-		if (line[i] == '\n') {
+		if (line[array_index] == '\n') {
 			/* Variables */
-			int index = i;
-			i = 0;
+			int index = array_index;
+			array_index = 0;
+			line_count++;
 
-			/* Handle Line */
-			parseLine(line, index+1);
+			/* Check parseLint result */
+			if (parseLine(line, index) > 80) {
+				lines_exceeding++;
+			}
+
+			/* Reset Array */
 			reset(line, 999);
-			continue;
 		}
 
 		/* Increment index */
-		i++;
+		array_index++;
 	}
+
+	/* Output Results */
+	printf("%d %d", line_count, lines_exceeding);
 
 	/* Return */
 	return 0;
@@ -64,20 +83,23 @@ int main(int argc, char *argv[])  {
  * @param char[] line array
  * @param int array size
  * @throws None
- * @author Alec M. <https://amattu.com>
+ * @author Alec M.
  * @date 2020-09-17T12:55:38-040
  */
-void parseLine(char l[], int size) {
+int parseLine(char l[], int size) {
 	/* Variables */
 	int length = parseWhitespace(l, size);
-	int i;
+	int array_index;
 
 	/* Output */
-	printf("%c %4d> ", (length > 80 ? *"X" : *" "), size-1);
-	for (i = 0; i < size; i++) {
+	printf("%c %4d> ", (length > 80 ? *"X" : *" "), size);
+	for (array_index = 0; array_index <= size; array_index++) {
 		/* Output */
-		printf("%c", l[i]);
+		printf("%c", l[array_index]);
 	}
+
+	/* Return */
+	return length;
 }
 
 /**
@@ -87,27 +109,42 @@ void parseLine(char l[], int size) {
  * @param int array size
  * @return int calculated length
  * @throws None
- * @author Alec M. <https://amattu.com>
+ * @author Alec M.
  * @date 2020-09-17T12:56:36-040
  */
 int parseWhitespace(char l[], int size) {
-	return 0;
+	/* Variables */
+	int array_index;
+	int length = 0;
+
+	/* Iterate Through Array */
+	for (array_index = 0; array_index <= size; array_index++) {
+		if (l[array_index] == '\t') {
+			length += 8 - (length % 8);
+		} else if (l[array_index] != '\n') {
+			length++;
+		}
+	}
+
+	/* Return */
+	return length;
 }
 
 /**
  * Reset given character array
+ *
  * @param char[] array
  * @param int array size
  * @throws None
- * @author Alec M. <https://amattu.com>
+ * @author Alec M.
  * @date 2020-09-17T13:04:53-040
  */
 void reset(char *array, int size) {
 	/* Variables */
-	int i;
+	int array_index;
 
-	/* Loops */
-	for (i = 0; i < size; i++) {
-		array[i] = '\0';
+	/* Iterate Through Array */
+	for (array_index = 0; array_index < size; array_index++) {
+		array[array_index] = '\0';
 	}
 }
