212dcc103575eaba3cec6021efb73204c22c886e
= <alecmattu1234@gmail.com>
2020-09-27 11:26:31 -0400

    Implement print_instruction and determine_opcode

--- projects/project3/machine.c
+++ projects/project3/machine.c
@@ -19,32 +19,61 @@
 */
 
 /*** REMOVE AFTER CONVERTING TABS->SPACES ***/
-/*** REMOVE AFTER FORMATTING encode_instruction PARAMETER DESIGN ***/
+/*** REMOVE AFTER FORMATTING LONG PARAMETER DESIGN ***/
 
 /* Files */
 #include <stdio.h>
 #include "machine.h"
 
 /* Prototypes */
-void print_opcode(int opcode);
+void print_opcode(Opcode opcode);
 void print_register(unsigned int bits);
 unsigned int read_bit(unsigned int byte, int msb, int lsb);
+Opcode determine_opcode(unsigned int opcode);
 
+/**
+ * Print a CPU instruction set
+ *
+ * @param unsigned int instruction
+ * @throws None
+ * @author Alec M. <https://amattu.com>
+ * @date 2020-09-27T11:15:39-040
+ */
 void print_instruction(Hardware_word instruction) {
-	print_opcode(instruction >> 28); /* opcode (Bytes 32-28) */
-	print_register(read_bit(instruction, 27, 23)); /* Reg 1 (27-23) */
-	print_register(read_bit(instruction, 22, 18)); /* Reg 2 (22-18) */
-	print_register(read_bit(instruction, 17, 13)); /* Reg 3 (17-13) */
-	read_bit(instruction, 12, 0); /* Mem Addr or Immedimate */
+	/* Variables */
+	Opcode opcode = determine_opcode(read_bit(instruction, 32, 28)); /* opcode (32-28) */
+	unsigned int register1 = read_bit(instruction, 27, 23); /* Reg 1 (27-23) */
+	unsigned int register2 = read_bit(instruction, 22, 18); /* Reg 2 (22-18) */
+	unsigned int register3 = read_bit(instruction, 17, 13); /* Reg 3 (17-13) */
+	unsigned int addr_or_const = read_bit(instruction, 12, 0); /* Address/Constant (12-0) */
 
-	/*
-	In order to determine if we read Reg1-Reg3, I have to determine the opcode first
-	the opcode tells you to ignore which registers
+	/* Print Opcode */
+	print_opcode(opcode);
+	if (opcode == HALT) {
+		return;
+	}
 
-	Use bit operators to move throughout the parameter data
+	/* Print Register 1 (Unless HALT, no checks needed) */
+	printf(" ");
+	print_register(register1);
 
-	Eg. print_instruction(0x41912000) > div R3 R4 R9
-	*/
+	/* Print Register 2 */
+	if (opcode != LI && opcode != LOAD && opcode != STORE) {
+		printf(" ");
+		print_register(register2);
+	}
+
+	/* Print Register 3 */
+	if (opcode != INV && opcode != NOT && opcode != MV && opcode != LI && opcode != LOAD && opcode != STORE) {
+		printf(" ");
+		print_register(register3);
+	}
+
+	/* Print Address/Constant */
+	if (opcode == CMP || opcode == LI || opcode == LOAD || opcode == STORE) {
+		printf(" ");
+		printf("%d", addr_or_const); /* THIS IS TEMPORARY.... NOT PARSED AT ALL */
+	}
 }
 
 unsigned int encode_instruction(unsigned short opcode, unsigned short reg1, unsigned short reg2, unsigned short reg3, unsigned int addr_or_constant, Hardware_word *const hw_word) {
@@ -104,56 +133,56 @@ unsigned int read_bit(unsigned int byte, int msb, int lsb) {
 /**
  * Print opcode descripton
  *
- * @param int opcode
+ * @param Opcode opcode
  * @throws None
  * @author Alec M. <https://amattu.com>
  * @date 2020-09-26T13:50:29-040
  */
-void print_opcode(int opcode) {
+void print_opcode(Opcode opcode) {
 	switch (opcode) {
-		case 0:
+		case HALT:
 			printf("halt");
 			break;
-		case 1:
+		case ADD:
 			printf("add");
 			break;
-		case 2:
+		case SUB:
 			printf("sub");
 			break;
-		case 3:
+		case MUL:
 			printf("mul");
 			break;
-		case 4:
+		case DIV:
 			printf("div");
 			break;
-		case 5:
+		case REM:
 			printf("rem");
 			break;
-		case 6:
+		case INV:
 			printf("inv");
 			break;
-		case 7:
+		case AND:
 			printf("and");
 			break;
-		case 8:
+		case OR:
 			printf("or");
 			break;
-		case 9:
+		case NOT:
 			printf("not");
 			break;
-		case 10:
+		case CMP:
 			printf("cmp");
 			break;
-		case 11:
+		case MV:
 			printf("mv");
 			break;
-		case 12:
+		case LI:
 			printf("li");
 			break;
-		case 13:
+		case LOAD:
 			printf("load");
 			break;
-		case 14:
+		case STORE:
 			printf("store");
 			break;
 		default:
@@ -173,66 +202,103 @@ void print_opcode(int opcode) {
 void print_register(unsigned int bits) {
 	switch (bits) {
 		case R0:
-			printf(" R0");
+			printf("R0");
 			break;
 		case R1:
-			printf(" R1");
+			printf("R1");
 			break;
 		case R2:
-			printf(" R2");
+			printf("R2");
 			break;
 		case R3:
-			printf(" R3");
+			printf("R3");
 			break;
 		case R4:
-			printf(" R4");
+			printf("R4");
 			break;
 		case R5:
-			printf(" R5");
+			printf("R5");
 			break;
 		case R6:
-			printf(" R6");
+			printf("R6");
 			break;
 		case R7:
-			printf(" R7");
+			printf("R7");
 			break;
 		case R8:
-			printf(" R8");
+			printf("R8");
 			break;
 		case R9:
-			printf(" R9");
+			printf("R9");
 			break;
 		case R10:
-			printf(" R10");
+			printf("R10");
 			break;
 		case R11:
-			printf(" R11");
+			printf("R11");
 			break;
 		case R12:
-			printf(" R12");
+			printf("R12");
 			break;
 		case R13:
-			printf(" R13");
+			printf("R13");
 			break;
 		case R14:
-			printf(" R14");
+			printf("R14");
 			break;
 		case R15:
-			printf(" R15");
+			printf("R15");
 			break;
 		case R16:
-			printf(" R16");
+			printf("R16");
 			break;
 		case R17:
-			printf(" R17");
+			printf("R17");
 			break;
 		case R18:
-			printf(" R18");
+			printf("R18");
 			break;
 		case R19:
-			printf(" R19");
+			printf("R19");
 			break;
 		default:
 			break;
 	}
+}
+
+Opcode determine_opcode(unsigned int opcode) {
+	switch (opcode) {
+		case 0:
+			return HALT;
+		case 1:
+			return ADD;
+		case 2:
+			return SUB;
+		case 3:
+			return MUL;
+		case 4:
+			return DIV;
+		case 5:
+			return REM;
+		case 7:
+			return INV;
+		case 8:
+			return AND;
+		case 9:
+			return OR;
+		case 10:
+			return NOT;
+		case 11:
+			return CMP;
+		case 12:
+			return MV;
+		case 13:
+			return LI;
+		case 14:
+			return LOAD;
+		case 15:
+			return STORE;
+	}
+
+	return HALT;
 }
