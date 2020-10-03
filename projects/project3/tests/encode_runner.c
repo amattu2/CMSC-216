3e8d5bb4e75680812bc2218889dbee703b654864
= <alecmattu1234@gmail.com>
2020-10-03 15:24:30 -0400

    Redesign runner

--- projects/project3/tests/encode_runner.c
+++ projects/project3/tests/encode_runner.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "machine.h"
-#include "machine.c"
 
 /**
  * A testrunner for print_instruction()
@@ -15,23 +14,11 @@ int main(void) {
 	/* Iterate through inputs */
 	while (feof(stdin) != 1) {
 		/* Variables */
-		unsigned short opcode, r1, r2, r3, addr;
+		unsigned int opcode, r1, r2, r3, addr;
 		unsigned int hw;
 
-		/* Read Opcode (0-14) */
-		scanf("Opcode: %d\n", &opcode);
-
-		/* Read Register 1 */
-		scanf("Register 1 (Or -1): %d\n", &r1);
-
-		/* Read Register 2 */
-		scanf("Register 2 (Or -1): %d\n", &r2);
-
-		/* Read Register 3 */
-		scanf("Register 1 (Or -1): %d\n", &r3);
-
-		/* Read Addr / Const */
-		scanf("Address / Const: %d\n", &addr)
+		/* Read Input */
+		printf("Enter instructions (op, r1, r2, r3, addr): %u %u %u %u %u", &opcode, &r1, &r2, &r3, &addr);
 
 		/* Output Instruction */
 		if (encode_instruction(opcode, r1, r2, r3, addr, &hw)) {
