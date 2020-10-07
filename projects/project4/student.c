549888e48887e3e054b796939ca41ccd4fbfd674
= <alecmattu1234@gmail.com>
2020-10-07 09:04:49 -0400

    Finalize project 4

--- projects/project4/student.c
+++ projects/project4/student.c
@@ -13,15 +13,10 @@
 
 /**
  * The purpose of this project is to
- * -
- * -
- * -
+ * - Utilize the Student structure
+ * - Make minor changes to Student struct
+ * - Implement getters/setters for student struct
 */
-
-/*************************************/
-/**** REMOVE AFTER TABS -> SPACES ****/
-/****    REMOVE AFTER 80C LINES   ****/
-/*************************************/
 
 /* Files */
 #include <string.h>
@@ -29,112 +24,113 @@
 
 /* Create a new student structure */
 Student new_student(const char name[], unsigned int id, float shoe_size) {
-	/* Variables */
-	Student student;
-	char student_name[40];
+  /* Variables */
+  Student student;
+  char student_name[40];
 
-	/* Checks */
-	if (!name) {
-		strcpy(student_name, "");
-	} else if (strlen(name) > 39) {
-		strncpy(student_name, name, 40);
-	} else {
-		strcpy(student_name, name);
-	}
+  /* Checks */
+  if (!name) {
+    strcpy(student_name, "");
+  } else if (strlen(name) > 39) {
+    strncpy(student_name, name, 40);
+  } else {
+    strcpy(student_name, name);
+  }
 
-	/* Assign Values */
-	strcpy(student.name, student_name);
-	student.id = id;
-	student.shoe_size = shoe_size;
+  /* Assign Values */
+  strcpy(student.name, student_name);
+  student.id = id;
+  student.shoe_size = shoe_size;
 
-	/* Return */
-	return student;
+  /* Return */
+  return student;
 }
 
 /* Assign new values to student structure */
-void init_student(Student *const student, const char name[], unsigned int id, float shoe_size) {
-	/* Variables */
-	char student_name[40];
+void init_student(Student *const student, const char name[], unsigned int id,
+                  float shoe_size) {
+  /* Variables */
+  char student_name[40];
 
-	/* Checks */
-	if (!student) {
-		return;
-	}
-	if (!name) {
-		strcpy(student_name, "");
-	} else if (strlen(name) > 39) {
-		strncpy(student_name, name, 40);
-	} else {
-		strcpy(student_name, name);
-	}
+  /* Checks */
+  if (!student) {
+    return;
+  }
+  if (!name) {
+    strcpy(student_name, "");
+  } else if (strlen(name) > 39) {
+    strncpy(student_name, name, 40);
+  } else {
+    strcpy(student_name, name);
+  }
 
-	/* Assign Values */
-	strcpy(student->name, student_name);
-	student->id = id;
-	student->shoe_size = shoe_size;
+  /* Assign Values */
+  strcpy(student->name, student_name);
+  student->id = id;
+  student->shoe_size = shoe_size;
 }
 
 /* Check student structure id against given id */
 unsigned int has_id(Student student, unsigned int id) {
-	/* Checks */
-	if (student.id != id) {
-		return 0;
-	}
+  /* Checks */
+  if (student.id != id) {
+    return 0;
+  }
 
-	/* Default */
-	return 1;
+  /* Default */
+  return 1;
 }
 
 /* Check student structure name against given name */
 unsigned int has_name(Student student, const char name[]) {
-	/* Checks */
-	if (!student.name) {
-		return 0;
-	}
-	if (strcmp(student.name, name) != 0) {
-		return 0;
-	}
+  /* Checks */
+  if (!student.name) {
+    return 0;
+  }
+  if (strcmp(student.name, name) != 0) {
+    return 0;
+  }
 
-	/* Default */
-	return 1;
+  /* Default */
+  return 1;
 }
 
 /* Get student structure id */
 unsigned int get_id(Student student) {
-	return student.id;
+  return student.id;
 }
 
 /* Get student structure shoe size */
 float get_shoe_size(Student student) {
-	return student.shoe_size;
+  return student.shoe_size;
 }
 
 /* Change student shoe size */
 Student change_shoe_size(Student student, float new_shoe_size) {
-	/* Variables */
-	student.shoe_size = new_shoe_size;
+  /* Variables */
+  student.shoe_size = new_shoe_size;
 
-	/* Return */
-	return student;
+  /* Return */
+  return student;
 }
 
 /* Change student name */
 void change_name(Student *const student, const char new_name[]) {
-	/* Variables */
-	char student_name[40];
+  /* Variables */
+  char student_name[40];
 
-	/* Checks */
-	if (!student) {
-		return;
-	}
-	if (!new_name) {
-		strcpy(student_name, "");
-	} else if (strlen(new_name) > 39) {
-		strncpy(student_name, new_name, 40);
-	} else {
-		strcpy(student_name, new_name);
-	}
+  /* Checks */
+  if (!student) {
+    return;
+  }
+  if (!new_name) {
+    strcpy(student_name, "");
+  } else if (strlen(new_name) > 39) {
+    strncpy(student_name, new_name, 40);
+  } else {
+    strcpy(student_name, new_name);
+  }
 
-	/* Assign Values */
-	strcpy(student->name, student_name);
+  /* Assign Values */
+  strcpy(student->name, student_name);
 }
