87019dc3e8c187d5749605221606bd7737e49aa7
= <alecmattu1234@gmail.com>
2020-10-09 18:09:26 -0400

    Setup project 5

---
+++ projects/project5/student.c
@@ -0,0 +1,23 @@
+/*
+ * CMSC 216 (Fall 2020)
+ * Section: 0303 (M/W 8am)
+ * University ID: 117568813
+ * Directory ID: amattu
+ *
+ * Author: Alec M.
+ * Date: Oct 10th, 2020
+ *
+ * I pledge on my honor that I have not given or received
+ * any unauthorized assistance on this assignment.
+*/
+
+/**
+ * The purpose of this project is to
+ * - Utilize the Student structure
+ * - Make minor changes to Student struct
+ * - Implement getters/setters for student struct
+*/
+
+/* Files */
+#include <string.h>
+#include "student.h"
