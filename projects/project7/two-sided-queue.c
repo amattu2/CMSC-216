4524f2118bd12284e1bf564f7d606f5642486074
= <alecmattu1234@gmail.com>
2020-10-17 14:17:58 -0400

    Implement init

--- projects/project7/two-sided-queue.c
+++ projects/project7/two-sided-queue.c
@@ -22,8 +22,30 @@
 
 /* Files */
 #include <stdio.h>
+#include <stdlib.h>
 #include "two-sided-queue.h"
 
-int main(void) {
-    return 0;
+/* Initialize queue struct */
+void init(Two_sided_queue *const twosq) {
+  /* Variables */
+  Two_sided_queue *queue;
+
+  /* Checks */
+  if (!twosq)
+    return;
+
+  /* Assign Variables */
+  if ((queue = malloc(sizeof(Node) + sizeof(Node) + sizeof(int)))) {
+    queue->head = NULL;
+    queue->tail = NULL;
+    queue->size = 0;
+    *twosq = *queue;
+  }
 }
+
+int add_front(Two_sided_queue *const twosq, int new_value);
+int add_back(Two_sided_queue *const twosq, int new_value);
+int num_elements(Two_sided_queue *const twosq);
+void print(Two_sided_queue *const twosq);
+int remove_front(Two_sided_queue *const twosq, int *value);
+int remove_back(Two_sided_queue *const twosq, int *value);
