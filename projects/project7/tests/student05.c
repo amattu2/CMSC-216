7f3884949cb3f645449f5e42d6f7a5c711ebd5d0
= <alecmattu1234@gmail.com>
2020-10-20 12:41:56 -0400

    Implement student test 05

---
+++ projects/project7/tests/student05.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <assert.h>
+#include "two-sided-queue.h"
+
+#define size 8
+
+/*
+ * Test adding to front/back rotationally
+ * Then removing elements
+*/
+int main(void) {
+  Two_sided_queue queue;
+  int elements[size]= {2, 6, 3, 8, 0, 1, 5, 7};
+  int i;
+  int temp;
+
+  init(&queue);
+
+  for (i = 0; i < size; i++) {
+    if (i % 2 == 0) {
+      add_front(&queue, elements[i]);
+    } else {
+      add_back(&queue, elements[i]);
+    }
+  }
+
+  assert(num_elements(&queue) == 8);
+
+  for (i = 0; i < size; i++) {
+    if (i % 2 == 0) {
+      remove_back(&queue, elements[i]);
+    } else {
+      remove_front(&queue, elements[i]);
+    }
+  }
+
+  assert(num_elements(&queue) == 0);
+
+  printf("Pass\n");
+
+  return 0;
+}
