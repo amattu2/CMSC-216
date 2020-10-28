b3124a31ed5ac6e737817dd5d2d551a5b9e273f7
= <alecmattu1234@gmail.com>
2020-10-28 08:31:37 -0400

    Fix structure implementations

--- projects/project8/wstring-graph-datastructure.h
+++ projects/project8/wstring-graph-datastructure.h
@@ -5,18 +5,35 @@
  * Directory ID: amattu
  *
  * Author: Alec M.
- * Date: Oct 27th, 2020
+ * Date: Oct 28th, 2020
  *
  * I pledge on my honor that I have not given or received
  * any unauthorized assistance on this assignment.
 */
 
-typedef struct node {
-  int vertex;
-  struct node* next;
-} Node;
+/* Structure for edge (road) */
+typedef struct edge {
+  int source;
+  int destination;
+  int weight;
+  struct edge *next;
+} Edge;
 
+/* Structure for vertex (point) */
+typedef struct vertex {
+  char *name;
+  struct vertex *next;
+  struct queue *edges;
+} Vertex;
+
+/* Structure for vertex edges */
+typedef struct queue {
+  int size;
+  struct node *head;
+} Queue;
+
+/* Structure for graph */
 typedef struct graph {
-  int vertexCount;
-  struct node** adjLists;
+  int verticies;
+  struct vertex **head;
 } WString_graph;
