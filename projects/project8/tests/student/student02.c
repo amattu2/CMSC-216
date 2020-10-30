f2f6bd1a87b788c33b7fc8dd6d25cfe46b4a982b
Alec M <alecmattu1234@gmail.com>
2020-10-30 10:38:05 -0400

    Add student test 02

---
+++ projects/project8/tests/student/student02.c
@@ -0,0 +1,38 @@
+/*
+ * CMSC 216 (Fall 2020)
+ * Section: 0303 (M/W 8am)
+ * University ID: 117568813
+ * Directory ID: amattu
+ *
+ * Author: Alec M.
+*/
+
+/**
+ * Test calling various functions on null/invalid values
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include "wstring-graph.h"
+
+int main() {
+  WString_graph graph;
+
+  init_graph(&graph);
+
+  /* Add a good edge, test count */
+  assert(add_edge(&graph, "one", "two", 1) == 1);
+  assert(num_vertices(&graph) == 2); /* one, two */
+  assert(add_edge(&graph, "one", "two", 1) == 1);
+  assert(num_vertices(&graph) == 2); /* same as before */
+  assert(add_edge(&graph, "two", "one", 1) == 1);
+  assert(num_vertices(&graph) == 2); /* same as before */
+  assert(add_edge(&graph, "one", "one", 1) == 1);
+  assert(add_edge(&graph, "one", "one", 1) == 0); /* Limit of one edge self->self */
+  assert(add_edge(&graph, "two", "two", 1) == 1);
+  assert(add_edge(&graph, "two", "two", 1) == 0); /* Limit of one edge self->self */
+
+  printf("Pass\n");
+
+  return 0;
+}
