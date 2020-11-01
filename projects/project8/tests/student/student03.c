e27a21b12f4c61dcc21ed485111c8e23e23d1c6b
Alec M <alecmattu1234@gmail.com>
2020-11-01 11:47:31 -0500

    Add empty student test

---
+++ projects/project8/tests/student/student03.c
@@ -0,0 +1,28 @@
+/*
+ * CMSC 216 (Fall 2020)
+ * Section: 0303 (M/W 8am)
+ * University ID: 117568813
+ * Directory ID: amattu
+ *
+ * Author: Alec M.
+*/
+
+/**
+ * Add a edge
+ * Check adding it again (Updating it's value)
+ * Check it's new value
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include "wstring-graph.h"
+
+int main() {
+  WString_graph graph;
+
+  init_graph(&graph);
+
+  printf("Pass\n");
+
+  return 0;
+}
