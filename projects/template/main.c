7e31c1f3d61cce0f7d82ac333eafc391bae83606
Alec M <alecmattu1234@gmail.com>
2020-11-07 16:47:30 -0500

    Add template project

---
+++ projects/template/main.c
@@ -0,0 +1,27 @@
+/*
+ * CMSC 216 (Fall 2020)
+ * Section: 0303 (M/W 8am)
+ * University ID: 117568813
+ * Directory ID: amattu
+ *
+ * Author: Alec M.
+ * Date:
+ *
+ * I pledge on my honor that I have not given or received
+ * any unauthorized assistance on this assignment.
+*/
+
+/**
+ * The purpose of this project is to
+ * -
+ * -
+ * -
+*/
+
+
+/* Files */
+#include <stdio.h>
+
+int main(void) {
+    return 0;
+}
