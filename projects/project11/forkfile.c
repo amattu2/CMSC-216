6dd90bec1396c281c7d7efde12be7db61ae5efa8
Alec M <alecmattu1234@gmail.com>
2020-11-17 14:53:35 -0500

    Begin read_forkfile implementation

--- projects/project11/forkfile.c
+++ projects/project11/forkfile.c
@@ -21,15 +21,48 @@
 
 /* Files */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "forkfile.h"
 #include "split.h"
 #include "safe-fork.h"
 #include "make-target.h"
 
-Forkfile read_forkfile(const char filename[]) {
-  struct Forkfile *f = NULL;
+/* Prototypes */
+static Rule *add_rule(Forkfile *ff, char *line);
+static int add_action(Forkfile *ff, Rule *rule, char *line);
 
-  return f;
+Forkfile read_forkfile(const char filename[]) {
+  /* Variables */
+  Forkfile ff = {0, NULL};
+  FILE *file = NULL;
+  int rule_open = 0;
+  char line[1001];
+  struct rule *last_rule = NULL;
+
+  /* Checks */
+  if (!filename)
+    return ff;
+  if (!(file = fopen(filename, "r")))
+    return ff;
+
+  /* Read Lines */
+  while (fgets(line, sizeof(line), file)) {
+    /* Checks */
+    if (!line[0] || line[0] == '\n' || line[0] == ' ')
+      continue;
+    if (line[0] == '\t' && rule_open) {
+      add_action(&ff, last_rule, line);
+      rule_open = 0;
+    } else {
+      last_rule = add_rule(&ff, line);
+      rule_open = 1;
+    }
+  }
+
+  /* Return */
+  fclose(file);
+  return ff;
 }
 
 int lookup_target(Forkfile forkfile, const char target_name[]) {
@@ -61,5 +94,41 @@ int is_older(const char filename1[], const char filename2[]) {
 }
 
 int do_action(Forkfile forkfile, int rule_num) {
+  return 0;
+}
+
+/* HELPER: Create a new rule */
+static Rule *add_rule(Forkfile *ff, char *line) {
+  /* Variables */
+  struct rule *r = NULL;
+  char *name = NULL;
+  int dependency_count = 0;
+  char *dependency = NULL;
+
+  /* Checks */
+  if (!ff || !(r = malloc(sizeof(Forkfile))))
+    return NULL;
+  if ((name = strtok(line, ":")) && (r->name = malloc(strlen(name) + 1))) {
+    r->name = name;
+  } else return r;
+  if ((dependency = strtok(line, " "))) {
+    while (dependency != NULL) {
+      printf("Dependecy: %s\n", dependency);
+      dependency = strtok(NULL, " ");
+      dependency_count++;
+      /* add head using a function.. dont use this function for that */
+    }
+    printf("DP count: %i\n", dependency_count);
+  } else {
+    r->dependency_count = 0;
+    r->dependency_head = NULL;
+  }
+
+  /* Default */
+  return r;
+}
+
+/* HELPER: Create a new rule action */
+static int add_action(Forkfile *ff, Rule *rule, char *line) {
   return 0;
 }
