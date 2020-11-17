76e45de5a78d5f5240256ae83e2274ff83b26350
Alec M <alecmattu1234@gmail.com>
2020-11-17 12:52:33 -0500

    Remove instructor test files

--- projects/project11/functions.c
+++
@@ -1,9 +0,0 @@
-#include "functions.h"
-
-/* (c) Larry Herman, 2020.  You are allowed to use this code yourself, but
- * not to provide it to anyone else.
- */
-
-int add(int a, int b) {
-   return a + b;
-}
