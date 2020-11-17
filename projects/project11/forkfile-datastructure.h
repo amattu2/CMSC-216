900280977aeb603cbbc9c552a7efd7c7bf589f1c
Alec M <alecmattu1234@gmail.com>
2020-11-17 12:52:14 -0500

    Setup template structure

---
+++ projects/project11/forkfile-datastructure.h
@@ -0,0 +1,20 @@
+/*
+ * CMSC 216 (Fall 2020)
+ * Section: 0303 (M/W 8am)
+ * University ID: 117568813
+ * Directory ID: amattu
+ *
+ * Author: Alec M.
+ * Date: Nov 17th, 2020
+ *
+ * I pledge on my honor that I have not given or received
+ * any unauthorized assistance on this assignment.
+*/
+
+/* Structure for edge (road) */
+#ifndef STRUCT_DEF
+  #define STRUCT_DEF
+  typedef struct {
+    int temp;
+  } Forkfile;
+#endif
