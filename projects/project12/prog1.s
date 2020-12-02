bc67b7859e1e5197a73ea142e73ea3b18317a2c8
Alec M <alecmattu1234@gmail.com>
2020-12-02 08:32:57 -0500

    Begin program 1

--- projects/project12/prog1.s
+++ projects/project12/prog1.s
@@ -14,4 +14,62 @@
 # - Print the result of base^exponent
 # - Terminate with the standard MIPS assembly call
 
+# Define global variables
+.data
+   base:  .word  0
+   exponent:  .word  0
+   i:  .word  0
+   ans:  .word  0
+
+
+# Main Program
+.text
 main:
+	# Set i = 1
+  li $t0, 1
+  sw $t0, i
+
+  # Get integer base
+  li $v0, 5
+  syscall
+  # Move base from $v0 to $t0
+  move $t0, $v0
+	# Move base from $t0 to base
+	sw $t0, base
+
+  # Get integer exponent
+  li $v0, 5
+  syscall
+  # Move exponent from $v0 to $t0
+  move $t0, $v0
+	# Move exponent from $t0 to exponent
+	sw $t0, exponent
+
+	# Begin loop
+	j loop
+
+loop:
+	# Load global variables
+	lw $t0, exponent # exponent
+	lw $t1, base # base
+	lw $t2, i # index
+	lw $t3, ans # answer
+
+	# if i > exponent
+	bgt $t2, $t0, exit
+
+	# ans *= base
+	mul $t3, $t3, $t1
+
+	# Next iteration
+	j loop
+
+exit:
+  # Print out answer
+  lw $t0, ans
+  move $a0, $t0
+  syscall
+
+  # End Program
+  li $v0, 10
+  syscall
