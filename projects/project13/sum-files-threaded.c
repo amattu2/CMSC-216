bfdfaf028ee0ad21abb9e02c6691608c1e0c2854
Alec M <alecmattu1234@gmail.com>
2020-12-09 15:05:21 -0500

    Begin multi-threading implementation

--- projects/project13/sum-files-threaded.c
+++ projects/project13/sum-files-threaded.c
@@ -15,40 +15,66 @@
  * The purpose of this project is to
  * - Convert sum-files.c to multi-threaded
  * - Spawn a new thread for each file
+ * - Not implement global variables
 */
 
 /* (c) Larry Herman, 2020.  You are allowed to use this code yourself, but
  * not to provide it to anyone else.
 */
 
+/* Files */
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+/* Prototypes */
+static void *sum_file_contents(void *filename);
+
+/* Main function */
 int main(int argc, char *argv[]) {
-  long one_file_sum, all_files_total= 0;
-  int i, n;
-  FILE *fp;
+  /* Variables */
+  pthread_t *threads;
+  long file_sum = 0;
+  int i = 0;
 
-  if (argc > 1) {
-    for (i= 1; i < argc; i++) {
-      fp= fopen(argv[i], "r");
-
-      /* silently ignore nonexistent files, or ones that there was an error
-         trying to open */
-      if (fp != NULL) {
-        one_file_sum= 0;
-        fscanf(fp, "%d", &n);
-
-        while (!feof(fp)) {
-          one_file_sum += n;
-          fscanf(fp, "%d", &n);
-        }
-
-        all_files_total += one_file_sum;
-      }
+  /* Checks */
+  if (argc > 1 && (threads = malloc(sizeof(pthread_t) * (argc - 1)))) {
+    for (i = 1; i < argc; i++) {
+      pthread_create(&threads[i], NULL, sum_file_contents, argv[i]);
     }
   }
 
-  printf("%ld\n", all_files_total);
+  /* Print Results */
+  printf("%ld\n", file_sum);
 
+  /* Default */
   return 0;
+}
+
+/* Sum the integer contents of a file */
+static void *sum_file_contents(void *filename) {
+  /* Variables */
+  FILE *file = NULL;
+  int *sum = NULL;
+  int number = 0;
+
+  /* Checks */
+  if ((sum = malloc(sizeof(int*))))
+    sum = 0;
+  else return 0;
+  if (!filename)
+    return sum;
+  if (!(file = fopen(filename, "r")))
+    return sum;
+
+  /* Read File */
+  fscanf(file, "%d", &number);
+  while (!feof(file)) {
+    sum += number;
+    fscanf(file, "%d", &number);
+  }
+
+  /* Return */
+  fclose(file);
+  return sum;
 }
